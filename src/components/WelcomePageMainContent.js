1aec97be575a34dbf3203a8e492a0366eb041db2
Abdelkarim Hajji <ahajji@c1r5p8.1337.ma>
2025-10-30 20:16:09 +0100

    i add close and open poupap

--- src/components/WelcomePageMainContent.js
+++ src/components/WelcomePageMainContent.js
@@ -1,14 +1,17 @@
 import Poupap from "../components/WelcomePagePoupap.js"
+import { useState } from "react"
 
 function MainContent()
 {
+    const [display, setDisplay] = useState(false)
+
     return (
         <>
         <div className='flex flex-col justify-center items-center h-screen w-screen bg-welcome bg-no-repeat bg-cover  z-[0] fixed top-0 left-0 text-center'>
             <div className='text-white'>
                 <p className='xl:text-[12rem] lg:text-[10rem] 960:text-[8rem]  text-[2rem] font-bold'>Swipe Right<span className="font-light">®</span></p>
             </div>
-            <div  className='text-white font-bold w-44 h-12 text-lg bg-wel-gradient-but cursor-pointer hover:text-black  transition-colors duration-300 960:flex hidden items-center justify-center rounded-3xl'>
+            <div  className='text-white font-bold w-44 h-12 text-lg bg-wel-gradient-but cursor-pointer hover:text-black  transition-colors duration-300 960:flex hidden items-center justify-center rounded-3xl' onClick={() => setDisplay(!display)}>
                 Create account
             </div>
             <div className="w-[100%] h-[250px]  absolute bottom-0  flex-col 960:hidden flex  items-center">
@@ -20,8 +23,7 @@ function MainContent()
                 </div>
             </div> 
         </div>
-        
-        <Poupap />
+         <Poupap display={display} setDisplay={setDisplay}/> 
         </>
     );
 }
